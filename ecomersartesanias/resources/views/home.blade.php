e1ffe7b62e69a9092e7578d8fdfaa1ff478e7b25
14030609 <mike.angyz@gmail.com>
2018-04-27 18:49:32 -0500

    proyecto ecommers

---
+++ ecomersartesanias/resources/views/home.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+    <link rel="stylesheet" type="text/css" href="{{'css/bootstrap.css'}}">
+    <link rel="stylesheet" type="text/css" href="{{'css/style.css'}}">
+    <link rel="stylesheet" type="text/css" href="{{'css/font-awesome.css'}}">
+    <link rel="stylesheet" type="text/css" href="{{'//fonts.googleapis.com/css?family=Ubuntu:400,300,300italic,400italic,500,500italic,700,700italic'}}">
+    <link rel="stylesheet" type="text/css" href="{{'//fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic'}}">
+    <link rel="stylesheet" href="{{'css/flexslider.css'}}" type="text/css" media="screen" property="" />
+
+
+</head>
+<body>
+
+</body>
+</html>
